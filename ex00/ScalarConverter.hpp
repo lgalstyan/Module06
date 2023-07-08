cdfe63d06c005476bc6666336e5fe2cf20c6025d
lgalstyan <larisagalstya@gmail.com>
2023-07-08 12:36:22 +0400

    start

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,17 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+class ScalarConverter
+{
+    public:
+        ScalarConverter();
+        ScalarConverter(const ScalarConverter& );
+        ScalarConverter& operator=(const ScalarConverter& );
+        ~ScalarConverter();
+
+        // static convert(std::string );
+    private:
+
+};
+
+#endif
