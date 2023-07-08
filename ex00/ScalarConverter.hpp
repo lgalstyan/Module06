f383cbf818868f4953ea912268f10d9032ef7594
lgalstyan <larisagalstya@gmail.com>
2023-07-08 15:29:08 +0400

    Add color and check param count

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,6 +1,16 @@
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
+#define RED		"\033[31m"
+#define RESET	"\033[0m"
+#define BLUE	"\033[1;34m"
+#define GREEN   "\x1B[32m"
+#define YELLOW  "\x1B[33m"
+#define KMAG    "\x1B[35m"
+#define KCYN    "\x1B[36m"
+
+#include "iostream"
+
 class ScalarConverter
 {
     public:
@@ -9,7 +19,8 @@ class ScalarConverter
         ScalarConverter& operator=(const ScalarConverter& );
         ~ScalarConverter();
 
-        // static convert(std::string );
+        static void convert(std::string );
+
     private:
 
 };
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,2 +1,11 @@
+#include "ScalarConverter.hpp"
 
-
+int main(int argc, char **argv)
+{
+    // ScalarConverter converter;
+    if (argc != 2)
+        std::cerr << KCYN << "Convertor takes 1 parameter\n" << RESET;
+    else
+        std::cout << YELLOW << "Convertor\n" << RESET;
+    return (0);
+}
