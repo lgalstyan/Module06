7bc9d5737f0d9d7d9b7c75d9a4bc9c9620c96eb8
lgalstyan <larisagalstya@gmail.com>
2023-07-09 12:21:09 +0400

    Any

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,15 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter(){}
+
+ScalarConverter::~ScalarConverter(){}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other)
+{
+
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& rhs)
+{
+    
+}
