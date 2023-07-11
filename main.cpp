08a839451ca7ea6545ae561654f224389e9081db
Larisa Galstyan <lgalstya@c2r17s5.42yerevan.am>
2023-07-11 17:34:44 +0400

    check 1 try

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
-// #include <iostream>
+#include <iostream>
+#include <cmath>
 
-// int main()
-// {
-//     char a = 'x';
-//     std::cout << a  << std::endl;
-//     return (0);
-// }
+int main()
+{
+
+    double a;
+    char szOrbits[] = "1.0f";
+    char *end = NULL;
+    a = strtod(szOrbits, &end);
+    std::cout << a << std::endl;
+
+
+    // char a = 'x';
+    // double b = 1.6;
+    // std::cout << a  << std::endl;
+    // std::cout << static_cast<int>(b) << std::endl;
+
+    // double number = 3.7;
+    // double roundedDown = floor(number);
+    
+    // std::cout << "Original number: " << number << std::endl;
+    // std::cout << "Rounded down: " << roundedDown << std::endl;
+    // int a = 1;
+    // std::cout << static_cast<float>(a) << std::endl;
+    
+    return (0);
+}
